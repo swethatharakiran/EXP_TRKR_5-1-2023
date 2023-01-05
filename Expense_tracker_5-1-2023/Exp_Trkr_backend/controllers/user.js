2170631ce67abebbfef531cb673dfda0ba8d9736
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-01-05 14:52:05 +0530

    Add files via upload

--- Expense_tracker_5-1-2023/Exp_Trkr_backend/controllers/user.js
+++ Expense_tracker_5-1-2023/Exp_Trkr_backend/controllers/user.js
@@ -1,19 +1,35 @@
 const User=require('../models/user');
 
 exports.postsignupform=async(req,res,next)=>{
-    try{
+    
     const username=req.body.username;
     const email=req.body.email;
     const password=req.body.password;
 
-    if(username ==undefined|| username.length===0||email==undefined ||email.length===0 || password==undefined||password.length===0){
+    if(username ==undefined|| username.length===0||email==undefined ||
+        email.length===0 || password==undefined||password.length===0){
         res.status(400).json({err:"null parameters , something is not entered"})
     }
-    else{
+    
     await User.create({username:username,email:email,password:password})
-   .then(()=>{res.json({message:"successfully created new user"})})
-    }
-   }catch(err){
-    res.send(err)}
-
+    .then(result=>{
+        
+        res.json({message:"successfully created new user"})
+    })
+    .catch(err=>{
+    res.send(err)
+   })
 }
+    
+    
+/*exports.postloginform=(req,res,next)=>{
+    try{
+    const email=req.body.email;
+    const password=req.body.password;
+    if(email==undefined||email.length===0||password==undefined||password.length===0){
+        res.status(400).json({msg:"fields should not be empty"});
+    }
+    }
+    catch(err){console.log(err)}
+
+}*/
