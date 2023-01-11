0eeb5ecc015b3036a47b8dac2d629a4894a2b64d
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-01-11 19:03:24 +0530

    Add files via upload

---
+++ Expense_tracker_5-1-2023/Exp_Trkr_backend/controllers/password.js
@@ -0,0 +1,7 @@
+const User=require('../models/user');
+const Exp=require('../models/expense');
+const sequelize = require('../util/database');
+
+exports.forgotpassword=async(req,res)=>{
+console.log("hi")
+}
