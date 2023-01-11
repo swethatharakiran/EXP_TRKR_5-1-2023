4e533f976e54fe06df3806c25e093141d7b26ceb
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-01-11 17:54:08 +0530

    optimization of code using queries

--- Expense_tracker_5-1-2023/Exp_Trkr_backend/controllers/premiumfeature.js
+++ Expense_tracker_5-1-2023/Exp_Trkr_backend/controllers/premiumfeature.js
@@ -1,34 +1,49 @@
 const User=require('../models/user');
 const Exp=require('../models/expense');
+const sequelize = require('../util/database');
 
 exports.getuserleaderboard=async(req,res,next)=>{
     try{
-        const users=await User.findAll();
-        const expenses=await Exp.findAll();
-        const useraggregateexpenses={};
-        console.log("FROM CONTROLLER");
+        const usersleaderboard=await User.findAll({
+            attributes:[
+                'id',
+            'username',
+            [sequelize.fn('sum',sequelize.col('expenses.amount')),'total']
+             ], //optimization-choosing selected fields from table
+            include:[
+                {
+                    model:Exp,
+                    attributes:[]
+
+                }
+            ],
+                group:['user.id'],
+                order:[['total','desc']]
+            
+        });
+        
+       // const useraggregateexpenses={};
+        //console.log("FROM CONTROLLER");
         //console.log(expenses)
-        expenses.forEach(expense=>{
+        //expenses.forEach(expense=>{
             //console.log("FROM FOREACH LOOP")
             //console.log(expense.dataValues.userId);
-            if(useraggregateexpenses[expense.dataValues.userId]){
-                useraggregateexpenses[expense.dataValues.userId]+=expense.amount;
-            }
-            else{
-                useraggregateexpenses[expense.dataValues.userId]=expense.amount;
-            }
-        })
-        var userleaderboarddetails=[];
-        users.forEach(user=>{
-            //console.log("USER DETAILS");
-            //console.log(user);
-            userleaderboarddetails.push({name:user.dataValues.username,total:useraggregateexpenses[user.dataValues.id]||0});
-            //console.log(user.dataValues.username);
-            //console.log(useraggregateexpenses[user.dataValues.id])
-        })
-        //console.log(useraggregateexpenses);
-        userleaderboarddetails.sort((a,b)=>b.total-a.total)
-        res.status(200).json(userleaderboarddetails);
+           // if(useraggregateexpenses[expense.dataValues.userId]){
+             //   useraggregateexpenses[expense.dataValues.userId]+=expense.amount;
+            //}
+            //else{
+              //  useraggregateexpenses[expense.dataValues.userId]=expense.amount;
+            //}
+        //})
+        //var userleaderboarddetails=[];
+       // users.forEach(user=>{
+            
+            //userleaderboarddetails.push({name:user.dataValues.username,total:useraggregateexpenses[user.dataValues.id]||0});
+            
+       // })
+      
+        //userleaderboarddetails.sort((a,b)=>b.total-a.total)
+        res.status(200).json(usersleaderboard);
     }
     catch(err){
         console.log(err);
